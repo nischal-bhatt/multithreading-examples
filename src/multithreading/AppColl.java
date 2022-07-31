f5cb0283318ea1d8cb3ec149d7a418d5134fcf10
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-07-31 13:44:10 +0800

    sda

---
+++ src/multithreading/AppColl.java
@@ -0,0 +1,46 @@
+package multithreading;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppColl {
+
+	public static void main(String[] args) {
+
+		List<Integer> nums = new ArrayList<>();
+
+		Thread t1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for (int i = 0; i < 1000; i++) {
+					nums.add(i);
+				}
+			}
+
+		});
+
+		Thread t2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for (int i = 0; i < 1000; i++) {
+					nums.add(i);
+				}
+			}
+
+		});
+
+		t1.start();
+		t2.start();
+
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+
+		}
+
+		System.out.println("size pf array " + nums.size());
+	}
+}
