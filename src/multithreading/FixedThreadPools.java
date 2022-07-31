a1b3f4a30790bf085f71ba71331bb6192523d343
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-07-31 11:59:43 +0800

    f

---
+++ src/multithreading/FixedThreadPools.java
@@ -0,0 +1,43 @@
+package multithreading;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class Work implements Runnable {
+
+	private int id;
+	
+	
+	
+	public Work(int id) {
+		this.id = id;
+	}
+
+
+
+	@Override
+	public void run() {
+		System.out.println("task with id "+id+" is in work - thread id : " + Thread.currentThread().getId());
+		long duration = (long)(Math.random() * 5);
+		try {
+			TimeUnit.SECONDS.sleep(duration);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
+
+public class FixedThreadPools {
+
+	public static void main(String[] args) {
+		
+		ExecutorService executor = Executors.newFixedThreadPool(10);
+	
+	    for (int i =0; i<100; i++)
+	    {
+	    	executor.execute(new Work(i+1));
+	    }
+	}
+}
