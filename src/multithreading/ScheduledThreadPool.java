2ff85983810366e218ecf63db46df810638fc740
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-07-31 12:04:46 +0800

    asd

---
+++ src/multithreading/ScheduledThreadPool.java
@@ -0,0 +1,24 @@
+package multithreading;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+class StockMarketUpdater implements Runnable{
+
+	@Override
+	public void run() {
+		System.out.println("updating and downloading stock related data from the web ...");
+	}
+	
+}
+
+public class ScheduledThreadPool {
+
+	public static void main(String[] args) {
+		
+		ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+	
+	    executor.scheduleAtFixedRate(new StockMarketUpdater(), 1000, 2000, TimeUnit.MILLISECONDS);
+	}
+}
