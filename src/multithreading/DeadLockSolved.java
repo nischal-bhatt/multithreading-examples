e798a5009e05df1f079363d5078ea60277b3143f
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-07-31 10:41:41 +0800

    deadlock solved and unsolved

---
+++ src/multithreading/DeadLockSolved.java
@@ -0,0 +1,52 @@
+package multithreading;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class DeadLockSolved {
+	private Lock lock1 = new ReentrantLock(true);
+	private Lock lock2 = new ReentrantLock(true);
+	
+	public static void main(String[] args) {
+		
+		DeadLockSolved deadLock = new DeadLockSolved();
+		
+		
+		new Thread(deadLock::worker1,"worker1").start();
+		new Thread(deadLock::worker2,"worker2").start();
+	}
+	
+	public void worker1() {
+		lock1.lock();
+		System.out.println("worker 1 acquires lock 1...");
+		try {
+			Thread.sleep(300);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		lock2.lock();
+		System.out.println("worker 1 acquired lock 2 ....");
+		
+		lock1.unlock();
+		lock2.unlock();
+	}
+	
+	public void worker2() {
+		lock1.lock();
+		System.out.println("worker 2 acquires lock 1...");
+		try {
+			Thread.sleep(300);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		lock2.lock();
+		System.out.println("worker 2 acquired lock 2 ....");
+		
+		lock1.unlock();
+		lock2.unlock();
+	}
+}
