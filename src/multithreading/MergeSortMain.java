3ff2851fb0f01a550b1ce40230a4ddbeb22f1a57
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-07-31 18:39:40 +0800

    asdads

--- src/multithreading/MergeSortMain.java
+++ src/multithreading/MergeSortMain.java
@@ -3,7 +3,12 @@ package multithreading;
 public class MergeSortMain {
 
 	public static void main(String[] args) {
-		int[] nums = {5,-1,0,7,2,3,2,1,0,1,2};
+	//	int[] nums = {5,-1,0,7,2,3,2,1,0,1,2};
+		
+		
+		int[] nums = {8,8,2,4,7};
+		
+		
 		MergeSort sort = new MergeSort(nums);
 		sort.sort();
 		sort.showArray();
